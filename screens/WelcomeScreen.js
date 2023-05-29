07124383e6235b587f8ac27a4ca5175fa14ec88a
hammadi azaiez <hamadi19191919@gmail.com>
2023-05-29 16:24:58 +0100

    admin page DONE

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -26,24 +26,7 @@ export default function WelcomeScreen() {
           for future projects. A one day challenge full of surprises for attendants and prices for
           champions
         </Text>
-        <Text className="text-white text-m p-3" style={{ height: 300 }}>
-          ENIGROBOTs is an innovative event organized by The National Engineering School of Gabes
-          with intention to promote the quality of research in the field of technology in Tunisia.
-          This event is an assembly of students coming to win the challenge using their wits and
-          spirits. It is a place to exchange knowledge, make memories ,form long lasting bonds of
-          friendship and also have the opportunity to cooperate with teams from other establishments
-          for future projects. A one day challenge full of surprises for attendants and prices for
-          champions
-        </Text>
-        <Text className="text-white text-m p-3" style={{ height: 300 }}>
-          ENIGROBOTs is an innovative event organized by The National Engineering School of Gabes
-          with intention to promote the quality of research in the field of technology in Tunisia.
-          This event is an assembly of students coming to win the challenge using their wits and
-          spirits. It is a place to exchange knowledge, make memories ,form long lasting bonds of
-          friendship and also have the opportunity to cooperate with teams from other establishments
-          for future projects. A one day challenge full of surprises for attendants and prices for
-          champions
-        </Text>
+
         <View className="space-y-4">
           <TouchableOpacity
             onPress={() => navigation.navigate("SignUp")}
--- screens/admin/Admin.js
+++ screens/admin/Admin.js
@@ -7,8 +7,12 @@ import { signUserOut, userAuth } from "../../components/userAuth";
 import { getData } from "../../functions";
 import UserRobots from "../client/UserRobots";
 import { AnimatedCircularProgress } from "react-native-circular-progress";
+import CircularProgress from "react-native-circular-progress-indicator";
+
 export default function Admin() {
   const user = userAuth();
+  const [viewList, setViewList] = useState(false);
+  const [viewPaymentList, setViewPaymentList] = useState(false);
   const navigation = useNavigation();
   const [robotList, setRobotList] = useState([]);
   const [paymentList, setPaymentList] = useState([]);
@@ -23,6 +27,26 @@ export default function Admin() {
       return a.type == "race";
     }).length,
   };
+
+  insights = {
+    robotsNumber: robotCounter.fighter + robotCounter.lineF + robotCounter.race,
+    registeredUser: new Set(
+      robotList.map((item) => {
+        return item.uid;
+      })
+    ).size,
+    registeredUniversities: new Set(
+      robotList.map((item) => {
+        return item.university;
+      })
+    ).size,
+    registeredUniClubs: new Set(
+      robotList.map((item) => {
+        return item.university;
+      })
+    ).size,
+  };
+  //console.log(insights);
   PaymentCounter = {
     fighter: robotList.filter((a) => {
       return a.payment && a.type == "fighter";
@@ -38,15 +62,6 @@ export default function Admin() {
     await getData("robots", setRobotList);
     await getData("payments", setPaymentList);
   }
-  useEffect(() => {
-    getAllData();
-    const backAction = () => {
-      console.log("pressed back");
-      return true;
-    };
-    const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
-    return () => backHandler.remove(); // Clean up the event listener
-  }, [user]);
   goals = {
     fighter: 25,
     lineF: 30,
@@ -58,6 +73,17 @@ export default function Admin() {
     race: (100 * PaymentCounter.race) / robotCounter.race,
   };
 
+  useEffect(() => {
+    getAllData();
+    const backAction = () => {
+      console.log("pressed back");
+      return true;
+    };
+    if (!!calculatedRates.fighter || calculatedRates.fighter === 0) setViewPaymentList(true);
+    const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
+    return () => backHandler.remove(); // Clean up the event listener
+  }, [user, calculatedRates.fighter]);
+
   return (
     <SafeAreaView className="flex flex-1" style={{ backgroundColor: themeColors.bg }}>
       <ScrollView>
@@ -67,7 +93,7 @@ export default function Admin() {
             style={{ width: 500, height: 200 }}
           />
         </View>
-        <Text className="text-white font-bold text-2xl text-center my-7">
+        <Text className="text-rose-800 font-bold text-2xl text-center my-7">
           welcome admin : {user?.displayName} !
         </Text>
 
@@ -124,78 +150,122 @@ export default function Admin() {
             backgroundColor="#6e6c6b"
           />
         </View>
-        <Text className="text-white font-bold text-l text-center mb-4 mt-10">Payment progress</Text>
-        <View className="flex flex-row justify-evenly ">
-          <Text className="text-white font-bold text-l">Line Follower</Text>
-          <Text className="text-white font-bold text-l mx-7">All Terrain</Text>
-          <Text className="text-white font-bold text-l">Fight Robots</Text>
-        </View>
-        <View className="flex flex-row justify-evenly mt-10">
-          <Text className="text-white font-bold text-l">
-            {PaymentCounter.lineF} / {robotCounter.lineF}
-          </Text>
-          <Text className="text-white font-bold text-l mx-8">
-            {PaymentCounter.race} / {robotCounter.race}
-          </Text>
-          <Text className="text-white font-bold text-l">
-            {PaymentCounter.fighter} / {robotCounter.fighter}
-          </Text>
-        </View>
-        <View className="flex flex-row justify-evenly -mt-14">
-          <AnimatedCircularProgress
-            key={4}
-            size={100}
-            width={15}
-            fill={calculatedRates.lineF}
-            rotation={-140}
-            arcSweepAngle={280}
-            tintColor={themeColors.logo}
-            duration={1000}
-            backgroundColor="#6e6c6b"
-          />
-          <AnimatedCircularProgress
-            key={5}
-            size={100}
-            width={15}
-            fill={calculatedRates.race}
-            rotation={-140}
-            arcSweepAngle={280}
-            tintColor={themeColors.logo}
-            duration={1000}
-            backgroundColor="#6e6c6b"
-          />
-          <AnimatedCircularProgress
-            key={6}
-            size={100}
-            width={15}
-            duration={1000}
-            fill={calculatedRates.fighter}
-            rotation={-140}
-            arcSweepAngle={280}
-            tintColor={themeColors.logo}
-            backgroundColor="#6e6c6b"
-          />
-        </View>
-        <Text className="text-white text-l font-bold p-3 mx-3 my-4">
-          Check your inscriptions below :
-        </Text>
-
-        <ScrollView>
-          {robotList.map((a) => {
-            robotId = a.id;
-            paymentInfo = paymentList.filter((item) => {
-              return item?.robotId == robotId ? true : false;
-            });
-            return (
-              <UserRobots
-                key={robotId}
-                data={a}
-                paymentInfo={paymentInfo[0]}
-                navigationEvent="RobotCheck"
+        {viewPaymentList && (
+          <View>
+            <Text className="text-white font-bold text-l text-center  mt-10 mb-3">
+              Payment progress
+            </Text>
+            <View className="flex flex-row justify-evenly ">
+              <Text className="text-white font-bold text-l">Line Follower</Text>
+              <Text className="text-white font-bold text-l mx-8">All Terrain</Text>
+              <Text className="text-white font-bold text-l">Fight Robots</Text>
+            </View>
+            <View className="flex flex-row justify-evenly mt-3 -mb-9">
+              <CircularProgress
+                radius={50}
+                value={calculatedRates.lineF}
+                textColor="#ffffff"
+                activeStrokeColor={calculatedRates.lineF >= 50 ? "#2ecc71" : themeColors.logo}
+                fontSize={15}
+                valueSuffix={"%"}
+                inActiveStrokeColor={calculatedRates.lineF >= 50 ? "#2ecc71" : "#6e6c6b"}
+                inActiveStrokeOpacity={0.2}
+                inActiveStrokeWidth={6}
+                duration={2000}
               />
-            );
-          })}
-        </ScrollView>
+              <CircularProgress
+                radius={50}
+                value={calculatedRates.race}
+                textColor="#ffffff"
+                fontSize={15}
+                valueSuffix={"%"}
+                activeStrokeColor={calculatedRates.race >= 50 ? "#2ecc71" : themeColors.logo}
+                inActiveStrokeColor={calculatedRates.race >= 50 ? "#2ecc71" : "#6e6c6b"}
+                inActiveStrokeOpacity={0.2}
+                inActiveStrokeWidth={6}
+                duration={2000}
+              />
+              <CircularProgress
+                radius={50}
+                value={calculatedRates.fighter}
+                activeStrokeColor={calculatedRates.fighter >= 50 ? "#2ecc71" : themeColors.logo}
+                textColor="#ffffff"
+                fontSize={15}
+                valueSuffix={"%"}
+                inActiveStrokeColor={calculatedRates.fighter >= 50 ? "#2ecc71" : "#6e6c6b"}
+                inActiveStrokeOpacity={0.2}
+                inActiveStrokeWidth={6}
+                duration={2000}
+              />
+            </View>
+            <View className="flex flex-row justify-evenly mt-10">
+              <Text className="text-white font-bold text-l">
+                {PaymentCounter.lineF} / {robotCounter.lineF}
+              </Text>
+              <Text className="text-white font-bold text-l mx-10">
+                {PaymentCounter.race} / {robotCounter.race}
+              </Text>
+              <Text className="text-white font-bold text-l">
+                {PaymentCounter.fighter} / {robotCounter.fighter}
+              </Text>
+            </View>
+          </View>
+        )}
+        <View className="space-y-4 mt-10">
+          <Text className="text-sm font-bold text-center text-white mb-7">
+            As a summary ,{" "}
+            <Text className="font-extrabold text-xl text-red-800"> {insights.registeredUser}</Text>{" "}
+            users have registered the total of{" "}
+            <Text className="font-extrabold text-xl text-red-800">{insights.robotsNumber}</Text>{" "}
+            robots from{" "}
+            <Text className="font-extrabold text-xl text-red-800">
+              {" "}
+              {insights.registeredUniClubs}
+            </Text>{" "}
+            clubs and
+            <Text className="font-extrabold text-xl text-red-800">
+              {" "}
+              {insights.registeredUniversities}
+            </Text>{" "}
+            universities
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              setViewList(!viewList);
+            }}
+            className="py-3  mx-7 rounded-xl"
+            style={{ backgroundColor: themeColors.logo }}
+          >
+            <Text className="text-xl font-bold text-center text-white">
+              {!viewList && "View list of registered robots"}
+              {viewList && "Hide list of registered robots"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {viewList && (
+          <ScrollView>
+            <Text className="text-white text-l font-bold p-3 mx-3 my-4">
+              Check your inscriptions below :
+            </Text>
+            <ScrollView>
+              {robotList.map((a) => {
+                robotId = a.id;
+                paymentInfo = paymentList.filter((item) => {
+                  return item?.robotId == robotId ? true : false;
+                });
+                return (
+                  <UserRobots
+                    key={robotId}
+                    data={a}
+                    paymentInfo={paymentInfo[0]}
+                    navigationEvent="RobotCheck"
+                  />
+                );
+              })}
+            </ScrollView>
+          </ScrollView>
+        )}
         <View className="space-y-4 my-10">
           <TouchableOpacity
             onPress={() => {
